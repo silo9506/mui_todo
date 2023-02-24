b1958ebe6e65c9a2903b2f4f0ffb8a64cc2ced55
DESKTOP-53PNB62\qudan <silo9506@gmail.com>
2023-02-24 19:44:30 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
